47f81d80665425e5b1df45e4a1dbb1ced26c33f3
bongolegend <nicomailbox@protonmail.com>
2024-05-19 01:48:40 +0000

    auto uuid on DB for testing

--- src/postgres/tables.py
+++ src/postgres/tables.py
@@ -1,13 +1,14 @@
-from sqlalchemy import Column, String, Text, Boolean, DateTime, ForeignKey, func
+from sqlalchemy import Column, String, Text, Boolean, DateTime, ForeignKey, func, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import UUID
+
 
 Base = declarative_base()
 
 class CommonBase(Base):
     __abstract__ = True
-    id = Column(UUID(as_uuid=True), primary_key=True, server_default="uuid_generate_v4()")
+    id = Column(UUID(as_uuid=True), primary_key=True, server_default=text("uuid_generate_v4()"))
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
 
