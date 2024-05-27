95146ab8b08e3977f1683ea1724e5302149d3bc3
bongolegend <nicomailbox@protonmail.com>
2024-05-27 22:32:23 +0000

    refactor main->api

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,4 @@
-from src import main
+from src import api
 from src.types import requests
 from src.postgres.connection import get_db
 
@@ -6,28 +6,28 @@ from src.postgres.connection import get_db
 def test_create_user():
     user = requests.NewUser(username='user1', email='user1@a.b')
     with get_db() as db:
-        db_user = main.create_user_if_new(db, user)
-        main.delete_user(db, db_user.id)
+        db_user = api.create_user_if_new(db, user)
+        api.delete_user(db, db_user.id)
 
 def test_get_user_by_email():
     with get_db() as db:
-        res = main.get_user_by_email(db, email='does-not-exist@a.b')
+        res = api.get_user_by_email(db, email='does-not-exist@a.b')
         assert res is None
 
 def test_create_follow():
     user1 = requests.NewUser(username='user1', email='user1@a.b')
     user2 = requests.NewUser(username='user2', email='user2@a.b')
     with get_db() as db:
-        user1 = main.create_user(db, user1)
-        user2 = main.create_user(db, user2)
+        user1 = api.create_user(db, user1)
+        user2 = api.create_user(db, user2)
 
         follow = requests.Follow(follower_id=user1.id, leader_id=user2.id)
-        main.create_follow(db, follow)
+        api.create_follow(db, follow)
         assert len(user1.is_following) == 1
         assert len(user1.followers) == 0
         assert len(user2.is_following) == 0
         assert len(user2.followers) == 1
 
-        main.delete_follow(db, follow)
-        main.delete_user(db, user1.id)
-        main.delete_user(db, user2.id)
+        api.delete_follow(db, follow)
+        api.delete_user(db, user1.id)
+        api.delete_user(db, user2.id)
