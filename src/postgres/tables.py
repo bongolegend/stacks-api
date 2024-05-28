f74fcc43fad9d2eb889591364479b8ad795f8eee
bongolegend <nicomailbox@protonmail.com>
2024-05-28 16:53:17 +0000

    create tables w sqla core

---
+++ src/postgres/tables.py
@@ -0,0 +1,41 @@
+from sqlalchemy import MetaData, Table, Column, String, Text, Boolean, DateTime, ForeignKey, func, text
+from sqlalchemy.dialects.postgresql import UUID
+
+metadata = MetaData()
+
+
+users = Table(
+    'users', metadata,
+    Column('id', UUID(as_uuid=True), primary_key=True, server_default=text("uuid_generate_v4()")),
+    Column('username', String, unique=True, nullable=False),
+    Column('email', String, unique=True, nullable=False),
+    Column('created_at', DateTime(timezone=True), server_default=func.now()),
+    Column('updated_at', DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+)
+
+follows = Table(
+    'follows', metadata,
+    Column('follower_id', UUID(as_uuid=True), ForeignKey('users.id', ondelete="CASCADE"), primary_key=True),
+    Column('leader_id', UUID(as_uuid=True), ForeignKey('users.id', ondelete="CASCADE"), primary_key=True)
+)
+
+goals = Table(
+    'goals', metadata,
+    Column('id', UUID(as_uuid=True), primary_key=True, server_default=text("uuid_generate_v4()")),
+    Column('user_id', UUID(as_uuid=True), ForeignKey('users.id', ondelete="CASCADE"), nullable=False),
+    Column('description', Text, nullable=False),
+    Column('is_completed', Boolean, default=False),
+    Column('created_at', DateTime(timezone=True), server_default=func.now()),
+    Column('updated_at', DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+)
+
+tasks = Table(
+    'tasks', metadata,
+    Column('id', UUID(as_uuid=True), primary_key=True, server_default=text("uuid_generate_v4()")),
+    Column('user_id', UUID(as_uuid=True), ForeignKey('users.id', ondelete="CASCADE"), nullable=False),
+    Column('goal_id', UUID(as_uuid=True), ForeignKey('goals.id'), nullable=True),
+    Column('description', Text, nullable=False),
+    Column('is_completed', Boolean, default=False),
+    Column('created_at', DateTime(timezone=True), server_default=func.now()),
+    Column('updated_at', DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+)
