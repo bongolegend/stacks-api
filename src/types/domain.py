436c9a64ca1874bed7aa09780c644470d1af89e6
bongolegend <nicomailbox@protonmail.com>
2024-05-29 00:27:29 +0000

    add domain.Brick

--- src/types/domain.py
+++ src/types/domain.py
@@ -1,7 +1,8 @@
 # The types.domain should be used for all interfaces internal to the stacks-api service
-from datetime import datetime
+from datetime import datetime, timezone
+from typing_extensions import Self
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
 from ulid import ULID
 from uuid import UUID
 
@@ -26,4 +27,17 @@ class Follow(requests.NewFollow):
     created_at: datetime | None = None
     updated_at: datetime | None = None
 
-    
+class Brick(BaseModel):
+    """A Brick is an entity in a feed. It can be a goal or a task"""
+    user: User
+    primary: Goal | Task
+    secondary: Goal | None = None
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+
+    @model_validator(mode="after")
+    def check_fields(self) -> Self:
+        if isinstance(self.primary, Goal) and self.secondary is not None:
+            raise ValueError('secondary must be None if primary is `domain.Goal`')
+        if isinstance(self.primary, Task) and self.secondary is None:
+            raise ValueError('secondary must be `domain.Goal` if primary is `domain.Task`')
+        return self
