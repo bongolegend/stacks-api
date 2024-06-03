5f16c26a32d22564356edf9c13a90e21e7c586b3
bongolegend <nicomailbox@protonmail.com>
2024-06-03 15:36:49 +0000

    fix sqla

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from src.postgres.tables import metadata as target_metadata
+from src.sqlalchemy.tables import metadata as target_metadata
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
