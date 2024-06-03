dbbfee8493adfb1b191d8653b2f706ed889c1bf4
bongolegend <nicomailbox@protonmail.com>
2024-06-03 20:37:49 +0000

    allow web app to work with cors

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ import logging
 from fastapi import FastAPI, Request
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.routes import common, v0
 
@@ -13,6 +14,16 @@ app = FastAPI()
 
 app.include_router(common.router)
 app.include_router(v0.router)
+
+# TODO hide this behind dev
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8081"],  # Allow specific origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
+
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
